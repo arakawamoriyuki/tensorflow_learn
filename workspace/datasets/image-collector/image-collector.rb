801328929c84f6c102f4f1e9419449b20b55e75d
新川 盛幸 <arakawa_moriyuki@ca-adv.co.jp>
2017-03-02 10:19:47 +0900

    fix image-collector

--- workspace/datasets/image-collector/image-collector.rb
+++ workspace/datasets/image-collector/image-collector.rb
@@ -10,7 +10,6 @@ def image_collector search
   driver = Selenium::WebDriver.for :chrome
   wait = Selenium::WebDriver::Wait.new(:timeout => 10)
   driver.navigate.to "https://www.google.co.jp/search?q=#{search}&tbm=isch"
-  nth_index = 20
   (1..Float::INFINITY).each do |nth_index|
     sleep SLEEP
     image_element = wait.until {
@@ -21,24 +20,21 @@ def image_collector search
     }
     driver.execute_script('arguments[0].scrollIntoView(true);', image_element)
     src = image_element.attribute('src')
+    blob = nil
     if (src =~ /^(http[s]?:\/\/.+).*$/) != nil
       open(src) do |string_io|
         blob = string_io.read
-        yield lambda { |path|
-          File.open(path, 'wb') do |file|
-            file.write blob
-          end
-        }, nth_index
       end
     elsif (src =~ /^.*;base64,.*$/) != nil
       blob = Base64.decode64(src.gsub(/^.*,/, ''))
+    end
+    unless blob.nil?
       yield lambda { |path|
         File.open(path, 'wb') do |file|
           file.write blob
         end
       }, nth_index
     end
-    nth_index += 1
   end
   driver.quit
 end
